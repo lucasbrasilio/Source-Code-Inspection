41cb68516518777af5e8e5f34193f0d1b36fe615
31405460 <31405460@092908-P31S401.macklabs.br>
2015-09-23 11:50:50 -0300

    Definição de mensagem de retorno

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,9 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+
+    public PapelMoedaInvalidaException() {
+        System.out.println("Papel-moeda inválida.");
+    }
     
 }
